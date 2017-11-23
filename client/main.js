8f25232e854465d9f56162d42a3bd7dfa0364834
Hobinx <binservmail@gmail.com>
2017-11-23 15:19:19 +0800

    add react-route history dependencies and add basic react route

--- client/main.js
+++ client/main.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import App from './components/app';
 import { Bins } from '../imports/collections/bins';
 
+const history = createBrowserHistory()
+
+const routes = (
+  <Router history={history}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+);
+
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.container'));
+  ReactDOM.render(routes, document.querySelector('.container'));
 });
